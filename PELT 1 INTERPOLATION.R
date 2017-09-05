610c695eb4fe089ef47bc44621b25ba9302b32b2
GitWrecked1 <eric.s.littlewood@gmail.com>
2017-09-05 17:35:58 -0400

    Pelt interpolation and analysis v 1.1

--- PELT 1 INTERPOLATION.R
+++ PELT 1 INTERPOLATION.R
@@ -30,12 +30,13 @@ if (getwd() == chartr("\\","/", as.character(Working_Directory))) {
     UC_THg <- PELT1_raw$THg_UC_ppm
   
   #check assumptions
-    hist(TC_THg, freq = FALSE, breaks = 20, xlab = "[THg] (ppm)") 
+    hist(TC_THg, freq = FALSE, breaks = 30, xlab = "[THg] (ppm)", 
+         main = "Histogram of TC [THg]") 
     curve(dnorm(x, mean(TC_THg), sd(TC_THg)), add=TRUE, col="darkblue", lwd=2)
     qqnorm(TC_THg, main = "PELT 1 TC [THg] normal Q-Q plot")
     qqline(TC_THg)
     
-    hist(UC_THg, freq = FALSE, breaks = 20, xlab = "[THg] (ppm)")
+    hist(UC_THg, freq = FALSE, breaks = 30, xlab = "[THg] (ppm)")
     curve(dnorm(x, mean(UC_THg), sd(UC_THg)), add=TRUE, col="darkblue", lwd=2)
     qqnorm(UC_THg, main = "PELT 1 UC [THg] normal Q-Q plot")
     qqline(UC_THg)
@@ -81,7 +82,7 @@ library(akima)
     #from z values
     TC_THg_interpMatrix <- interp(THg_TC_df$x,THg_TC_df$y, THg_TC_df$z, 
                                   #use spline interpolation
-                                  linear = FALSE, 
+                                  linear = TRUE, extrap = FALSE,
                                   #RASTER RESOLUTION
                                   #number of divisions of x range
                                   nx = 28, 
@@ -98,7 +99,7 @@ library(akima)
     
     #create raster image file with TC [THg] data
     image.plot(TC_THg_interpMatrix, xlim = range(THg_TC_df$x), ylim = range(THg_TC_df$y), 
-               col = viridis(20), 
+               zlim = range(THg_UC_df$z), col = viridis(20), 
                #main plot parameters
                main = "PELT 1 TC THg distribution", xlab = "X distance from origin (cm)", 
                ylab = "Y distance from origin (cm)", useRaster = TRUE, asp = 1, 
@@ -154,7 +155,7 @@ library(akima)
     #from z values
     UC_THg_interpMatrix <- interp(THg_UC_df$x,THg_UC_df$y, THg_UC_df$z, 
                                   #use spline interpolation
-                                  linear = FALSE, 
+                                  linear = TRUE, extrap = FALSE,
                                   #RASTER RESOLUTION
                                   #number of divisions of x range
                                   nx = 28, 
@@ -166,7 +167,7 @@ library(akima)
     
     #create raster image file with UC [THg] data
     image.plot(UC_THg_interpMatrix, xlim = range(THg_UC_df$x), ylim = range(THg_UC_df$y),
-               col = viridis(20), 
+               zlim = range(THg_UC_df$z), col = viridis(20), 
                #main plot parameters
                main = "PELT 1 UC THg distribution", xlab = "X distance from origin (cm)", 
                ylab = "Y distance from origin (cm)", useRaster = TRUE, asp = 1, 
@@ -207,11 +208,201 @@ library(akima)
            pch = 2) 
     
       #++Fur_region legend####
-      legend(x = c(25,70), y = c(-20,30), legend = c("Dorsal", "Ventral", "Fur Line"), 
+      legend(x = c(25,70), y = c(-20,30), legend = c("Dorsal", "Ventral", 
+                                                     "Fur Line"), 
              pch = c(1,0,2), title = "Fur region", horiz = FALSE,
              #text parameters 
              text.width = 2, xjust = 0)
 
-
+#***********************************************************************************
 #STATISTICAL ANALYSES####
-    anova()
+
+  #+One-way ANOVA for Anatomical_region factor####
+    
+    #++Residual diagnostics for TC [THg]####
+      
+      par(mfrow = c(2,2), mar = c(5,5,5,5))
+      
+      pelt1_TC_anova1 <- lm(THg_TC_ppm ~ Anatomical_region, data = PELT1_raw)
+      plot(pelt1_TC_anova1, main = "TC [THg] by anatomical region")
+      
+        #normality test of residuals
+        shapiro.test(residuals(pelt1_TC_anova1))
+        
+        par(mfrow = c(1,1), las = 1, mar = c(5,5,5,5))
+        hist(residuals(pelt1_TC_anova1), breaks = 30, freq = FALSE, 
+             main = "Histogram of PELT 1 TC [THg] one-way ANOVA residuals by anatomical region")
+        curve(dnorm(x, mean(residuals(pelt1_TC_anova1)), sd(residuals(pelt1_TC_anova1))), 
+              add=TRUE, col="darkblue", lwd=2)
+        
+          
+        #homoscedasticity test of residuals
+        library(car)
+        leveneTest(THg_TC_ppm ~ Anatomical_region, data = PELT1_raw)
+        
+        #pelt anova results
+        summary(pelt1_TC_anova1)
+        
+      #Tukey's multiple comparison
+        #set up ANOVA 
+        pelt1_TC_anova1.ANOVA <- aov(THg_TC_ppm ~ Anatomical_region, data = PELT1_raw)
+        
+        #Tukey's multiple comparison
+        TukeyHSD(pelt1_TC_anova1.ANOVA)
+        
+        #plot Tukey's
+        library(multcomp)
+          
+        pelt1_TC_anova1.means <- glht(pelt1_TC_anova1.ANOVA, 
+                                      linfct = mcp(Anatomical_region = "Tukey"))
+          
+        CI_pelt1_TC_anova1.means <- cld(pelt1_TC_anova1.means)
+          
+          #plot settings
+          par(mar = c(5,5,9,5))
+          plot(CI_pelt1_TC_anova1.means, 
+                 main = "Tukey's multiple comparison for PELT 1 TC [THg] by anatomical region",
+                 xlab = "Anatomical region", ylab = "[THg] (ppm)")
+      
+ 
+    #++Residual diagnostics for UC [THg]####
+      
+        par(mfrow = c(2,2), mar = c(5,5,5,5))
+        
+        pelt1_UC_anova1 <- lm(THg_UC_ppm ~ Anatomical_region, data = PELT1_raw)
+        plot(pelt1_UC_anova1, main = "UC [THg] by anatomical region")
+        
+        #normality test of residuals
+        shapiro.test(residuals(pelt1_UC_anova1))
+        
+        par(mfrow = c(1,1), las = 1, mar = c(5,5,5,5))
+        hist(residuals(pelt1_UC_anova1), breaks = 30, freq = FALSE, 
+             main = "Histogram of PELT 1 UC [THg] one-way ANOVA residuals by anatomical region")
+        curve(dnorm(x, mean(residuals(pelt1_UC_anova1)), sd(residuals(pelt1_UC_anova1))), 
+              add=TRUE, col="darkblue", lwd=2)
+        
+        
+        #homoscedasticity test of residuals
+        library(car)
+        leveneTest(THg_UC_ppm ~ Anatomical_region, data = PELT1_raw)
+        
+        #pelt anova results
+        summary(pelt1_UC_anova1)
+        
+      #Tukey's multiple comparison
+        #set up ANOVA 
+        pelt1_UC_anova1.ANOVA <- aov(THg_UC_ppm ~ Anatomical_region, data = PELT1_raw)
+        
+        #Tukey's multiple comparison
+        TukeyHSD(pelt1_UC_anova1.ANOVA)
+        
+        
+        #plot Tukey's
+        library(multcomp)
+        
+        pelt1_UC_anova1.means <- glht(pelt1_UC_anova1.ANOVA, 
+                                      linfct = mcp(Anatomical_region = "Tukey"))
+        
+        CI_pelt1_UC_anova1.means <- cld(pelt1_UC_anova1.means)
+        
+          #plot settings
+          par(mar = c(5,5,9,5))
+          plot(CI_pelt1_UC_anova1.means, 
+               main = "Tukey's multiple comparison for PELT 1 UC [THg] by anatomical region",
+               xlab = "Anatomical region", ylab = "[THg] (ppm)")
+    
+#*****    
+  #+One-way ANOVA for Fur_region factor####
+      
+    #++Residual diagnostics for TC [THg]####
+        
+        par(mfrow = c(2,2), mar = c(5,5,5,5))
+        
+        pelt1_TC_anova2 <- lm(THg_TC_ppm ~ Fur_region, data = PELT1_raw)
+        plot(pelt1_TC_anova2, main = "TC [THg] by fur region")
+        
+        #normality test of residuals
+        shapiro.test(residuals(pelt1_TC_anova2))
+        
+        par(mfrow = c(1,1), las = 1, mar = c(5,5,5,5))
+        hist(residuals(pelt1_TC_anova2), breaks = 30, freq = FALSE, 
+             main = "Histogram of PELT 1 TC [THg] one-way ANOVA residuals by fur region")
+        curve(dnorm(x, mean(residuals(pelt1_TC_anova2)), sd(residuals(pelt1_TC_anova2))), 
+              add=TRUE, col="darkblue", lwd=2)
+        
+        
+        #homoscedasticity test of residuals
+        library(car)
+        leveneTest(THg_TC_ppm ~ Fur_region, data = PELT1_raw)
+        
+        #pelt anova results
+        summary(pelt1_TC_anova2)
+        
+      #Tukey's multiple comparison
+        #set up ANOVA 
+        pelt1_TC_anova2.ANOVA <- aov(THg_TC_ppm ~ Fur_region, data = PELT1_raw)
+        
+        #Tukey's multiple comparison
+        TukeyHSD(pelt1_TC_anova2.ANOVA)
+        
+        #plot Tukey's
+        library(multcomp)
+        
+        pelt1_TC_anova2.means <- glht(pelt1_TC_anova2.ANOVA, 
+                                      linfct = mcp(Fur_region = "Tukey"))
+        
+        CI_pelt1_TC_anova2.means <- cld(pelt1_TC_anova2.means)
+        
+          #plot settings
+          par(mar = c(5,5,9,5))
+          plot(CI_pelt1_TC_anova2.means, 
+               main = "Tukey's multiple comparison for PELT 1 TC [THg] by fur region",
+               xlab = "Fur region", ylab = "[THg] (ppm)")
+        
+        
+    #++Residual diagnostics for UC [THg]####
+        
+        par(mfrow = c(2,2), mar = c(5,5,5,5))
+        
+        pelt1_UC_anova2 <- lm(THg_UC_ppm ~ Fur_region, data = PELT1_raw)
+        plot(pelt1_UC_anova2, main = "UC [THg] by fur region")
+        
+        #normality test of residuals
+        shapiro.test(residuals(pelt1_UC_anova2))
+        
+        par(mfrow = c(1,1), las = 1, mar = c(5,5,5,5))
+        hist(residuals(pelt1_UC_anova2), breaks = 30, freq = FALSE, 
+             main = "Histogram of PELT 1 UC [THg] one-way ANOVA residuals by fur region")
+        curve(dnorm(x, mean(residuals(pelt1_UC_anova2)), sd(residuals(pelt1_UC_anova2))), 
+              add=TRUE, col="darkblue", lwd=2)
+        
+        
+        #homoscedasticity test of residuals
+        library(car)
+        leveneTest(THg_UC_ppm ~ Fur_region, data = PELT1_raw)
+        
+        #pelt anova results
+        summary(pelt1_UC_anova2)
+        
+      #Tukey's multiple comparison
+        #set up ANOVA 
+        pelt1_UC_anova2.ANOVA <- aov(THg_UC_ppm ~ Fur_region, data = PELT1_raw)
+        
+        #Tukey's multiple comparison
+        TukeyHSD(pelt1_UC_anova2.ANOVA)
+        
+        
+        #plot Tukey's
+        library(multcomp)
+        
+        pelt1_UC_anova2.means <- glht(pelt1_UC_anova2.ANOVA, 
+                                      linfct = mcp(Fur_region = "Tukey"))
+        
+        CI_pelt1_UC_anova2.means <- cld(pelt1_UC_anova2.means)
+        
+          #plot settings
+          par(mar = c(5,5,9,5))
+          plot(CI_pelt1_UC_anova2.means, 
+               main = "Tukey's multiple comparison for PELT 1 UC [THg] by fur region",
+               xlab = "Fur region", ylab = "[THg] (ppm)")
+        
